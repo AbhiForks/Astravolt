182a5e728bd9c8a088320ccd61b852ec0657dc49
Abhilash <abhilashvishwa12@gmail.com>
2025-04-03 13:12:06 +0530

    Timestamp - 48.37 'gsap installation & setup'

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,46 +1,81 @@
 import { useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
-  const [hasClicked, setHasClicked] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
-  const [loadedVideos, setLoadedVideos] = useState(0)
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
 
   const totalVideos = 3;
-  const nextVideoRef = useRef(null)
+  const nextVideoRef = useRef(null);
 
   const handleVideoLoad = () => {
-    setLoadedVideos((prev) => prev + 1 );
-  }
+    setLoadedVideos((prev) => prev + 1);
+  };
 
   const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
 
   const handleMiniVdClick = () => {
-    setHasClicked(true)
+    setHasClicked(true);
     setCurrentIndex(upcomingVideoIndex);
-  }
+  };
 
-  const getVideoSrc = (index) => `videos/hero-${index}.mp4`
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
       <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
         <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
           <div onClick={handleMiniVdClick} className="origin-center scale-5 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
-            <video 
-            ref={nextVideoRef}
-            src={getVideoSrc(currentIndex + 1)}
-            loop
-            muted
-            id = "current-video"
-            className="size-69 origin-center scale-150 object-cover"
-            onLoadedData={handleVideoLoad}
+            <video
+              ref={nextVideoRef}
+              src={getVideoSrc(upcomingVideoIndex)}
+              loop
+              muted
+              id="current-video"
+              className="size-64 origin-center scale-150 object-cover"
+              onLoadedData={handleVideoLoad}
             />
           </div>
         </div>
+        <video
+          ref={nextVideoRef}
+          src={getVideoSrc(currentIndex)}
+          loop
+          muted
+          id="next-video"
+          className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+          onLoadedData={handleVideoLoad}
+        />
+        <video
+          src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+          autoPlay
+          loop
+          muted
+          className="absolute left-0 top-0 size-full object-cover object-center"
+          onLoadedData={handleVideoLoad}
+        />
       </div>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-100">
+        <b>Dy</b>namics
+      </h1>
+      <div className="absolute left-0 top-0 z-40 size-full">
+        <div className="mt-24 px-5 sm:px-10">
+          <h1 className="special-font hero-heading text-blue-100"><b>Astro</b>volt</h1>
+          <p className="mb-5 max-w-64 font-robert-regula text-blue-100">Experience the Epic <br /></p>
+          <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-500 flex-center gap-1" />
+        </div>
+      </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5  text-black">
+        <b>Dy</b>namics
+      </h1>
+
     </div>
   );
 }
+
 export default Hero;
 
